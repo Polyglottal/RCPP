4efc5010a407e6f08880a79c99659cc48c646ea3
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-11-09 23:27:42 +0900

    build: complex parser simultaneously builds collocations

--- Study/testProgram.cpp
+++ Study/testProgram.cpp
@@ -29,26 +29,87 @@
 // Instead the copy is made by char c in the for loop, so remove the
 // & pass-by-reference on char& c otherwise it conflicts with const.
 
-void parser(const std::string& text, std::vector<std::string>& wordlist)
+// But imagine if we could build the collocation lists here! It could be done in one go!
+void parser(
+    const std::string& text, 
+    std::vector<std::string>& wordlist, 
+    std::vector<std::string>& collList,
+    std::vector<std::string>& threeBundleList,
+    std::vector<std::string>& fourBundleList
+)
 {
-    std::string word { "" };
+    std::string word { };
+    std::string coll { };
+    std::string threeBundle { };
+    std::string fourBundle { };
+
+    int collCounter { 0 };
+    int threeBundleCounter { 0 };
+    int fourBundleCounter { 0 };
+
     for (char c : text)
-    {
+    {   
         if (c != '.')
         {
             word += c;
+            coll += c;
+            threeBundle += c;
+            fourBundle += c;
         }
 
         if (c == ' ')
         {
+            collCounter++;
+            threeBundleCounter++;
+            fourBundleCounter++;
+
             wordlist.push_back(word);
-            word = "";
+            word = {};
+
+            if (collCounter == 2)
+            { 
+                collList.push_back(coll);
+                coll = {};
+                collCounter = 0;
+            }
+
+            if (threeBundleCounter == 3)
+            {
+                threeBundleList.push_back(threeBundle);
+                threeBundle = {};
+                threeBundleCounter = 0;
+            }
+
+            if (fourBundleCounter == 4)
+            {
+                fourBundleList.push_back(fourBundle);
+                fourBundle = {};
+                fourBundleCounter = 0;
+            }
         }
     }
 
+    std::cout << "Word list:" << std::endl;
     for (int index {0}; index < static_cast<int>(wordlist.size()); index++)
     {
-        std::cout << wordlist[index];
+        std::cout << wordlist[index] << std::endl;
+    }
+    std::cout << "Collocation list" << std::endl;
+    for (int index {0}; index < static_cast<int>(collList.size()); index++)
+    {
+        std::cout << collList[index] << std::endl;
+    }
+
+    std::cout << "Lexical 3 bundles:" << std::endl;
+    for (int index {0}; index < static_cast<int>(threeBundleList.size()); index++)
+    {
+        std::cout << threeBundleList[index] << std::endl;
+    }
+
+    std::cout << "Lexical four bundles:" << std::endl;
+    for (int index {0}; index < static_cast<int>(fourBundleList.size()); index++)
+    {
+        std::cout << fourBundleList[index] << std::endl;
     }
     
 }
@@ -78,8 +139,8 @@ int main ()
     std::vector<std::string> collList { };
     std::vector<std::string> threeBundleList { };
     std::vector<std::string> fourBundleList { };
-    parser(text, wordlist);
-    collocationBuilder(wordlist, collList, 2);
-    collocationBuilder(wordlist, threeBundleList, 3);
-    collocationBuilder(wordlist, fourBundleList, 4);
+    parser(text, wordlist, collList, threeBundleList, fourBundleList);
+    //collocationBuilder(wordlist, collList, 2);
+    //collocationBuilder(wordlist, threeBundleList, 3);
+    //collocationBuilder(wordlist, fourBundleList, 4);
 }
